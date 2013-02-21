0ebbb5d4579012a4d62f9237743074f03eca6c21
Nick Cox <nickcox1008@gmail.com>
2013-02-21 12:36:46 -0700

    more functions!!!

--- msg.go
+++ msg.go
@@ -6,8 +6,23 @@ import (
 
 var Verbose *bool
 
-func Log(msg string, args ...interface{}) {
+// Uses fmt.Print
+func Log(msgs ...interface{}) {
+	if *Verbose {
+		fmt.Print(msgs...)
+	}
+}
+
+// Uses fmt.Printf
+func Logf(msg string, args ...interface{}) {
 	if *Verbose {
 		fmt.Printf(msg, args...)
+	}
+}
+
+// Uses fmt.Println
+func Logln(msgs ...interface{}) {
+	if *Verbose {
+		fmt.Println(msgs...)
 	}
 }
